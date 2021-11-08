22e8ab9dc52fed82e118a73aa546346190bbc4fb
Firstname Lastname <92145467+mnsd11@users.noreply.github.com>
2021-11-08 20:41:27 +0400

    update

---
+++ gpsNavigation/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  gpsNavigation
+//
+//  Created by DIRECTOR on 08.11.2021.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager ()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        startLocationManager()
+    }
+
+    func startLocationManager() {
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            
+            locationManager.desiredAccuracy=kCLLocationAccuracyHundredMeters
+            
+            locationManager.pausesLocationUpdatesAutomatically=false
+            
+            locationManager.startUpdatingLocation()
+        }
+    
+    }
+    
+
+}
+
+//extension ViewController: CLLocationManagerDelegate {
+  //  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    //    if let lastLocation = locations.last {
+      //      print(lastLocation.coordinate.latitude, lastLocation.coordinate.longitude)
+//        }
+  //  }
+//}
+ 
